c8ab534ea447c804dbc3daf1120e49da7114508e
Frank Riezebos <frankriezebos@themindoffice.nl>
2021-12-23 16:59:18 +0100

    socialize page, enjoy and enough

--- js/forms.js
+++ js/forms.js
@@ -1,68 +1,36 @@
 export function glasses() {
     var amount = $('#amount');
     var limit = $('#limit');
+    
+    // if amount is lower than limit, accept plus
+    $('.plus').click(function() {
+        if ( amount.val() < limit.val() ) {
+            console.log('amount is below 8');
 
-    $('.minus').click(function() {
-        var calc = +amount.val() - 1;
-        $("#amount").val(calc);
+            
+            var calc = +amount.val() + 1;
+            amount.val(calc);
+
+            console.log(amount.val());
+        }
     });
 
-    
-
-    amount.val().change(function() {
-        if ( amount.val() <= limit.val() ) {
-
-            console.log('amount is 8 or below');
-
-            $('.plus').click(function() {
-                var calc = +amount.val() + 1;
-                $("#amount").val(calc);
-            });
-        } else {
-            console.log('amount is above 8');
+    // if amount is 0 or higher, accept minus
+    $('.minus').click(function() {
+        if ( amount.val() > 0 ) {
+            var calc = +amount.val() - 1;
+            amount.val(calc);
+            console.log(amount.val());
         }
-    }
+    });
 
 
+    // if limit has been reached, show check icon
+    $('.plus').click(function() {
+        if ( amount.val() == limit.val() ) {
+            $('.plus, .minus').addClass('hide');
+            $('.finished').addClass('show');
+        }
+    });
 
-    // $('.plus').bind('click', {increment: 1}, incrementText);
-    // $('.minus').bind('click', {increment: -1}, incrementText);
-
-
-    // $('.plus, .minus').on("click", function() {
-    //     console.log(valueElement.text());
-    //     console.log('limiet', limit);
-
-    //     if ( valueElement.text() >= limit ) {
-    //         $('.plus').bind('click', {
-    //             increment: 0
-    //         }, incrementText);
-    //     } else {
-    //         $('.plus').bind('click', {
-    //             increment: 1
-    //         }, incrementText);
-    //     }
-    // });
-
-
-
-
-
-
-
-        // value (forms)
-    // function incrementValue(e){
-    //     valueElement.val(Math.max(parseInt(valueElement.val()) + e.data.increment, 0));
-    //     return false;
-    // }
-
-    // $('.plus').bind('click', {increment: 1}, incrementValue);
-    // $('.minus').bind('click', {increment: -1}, incrementValue);
-
-    
-    // // just text (no form)
-    // function incrementText(e){
-    //     valueElement.text(Math.max(parseInt(valueElement.text()) + e.data.increment, 0));
-    //     return false;
-    // }
 }
